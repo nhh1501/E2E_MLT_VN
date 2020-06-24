f4c6aa65a89967515de061dcbb9adfc64930b8f8
nhh1501 <nhh.1501@gmail.com>
2020-06-24 23:22:36 +0700

    Add files via upload

---
+++ net_utils.py
@@ -0,0 +1,50 @@
+'''
+Created on Aug 31, 2017
+
+@author: Michal.Busta at gmail.com
+'''
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+def np_to_variable(x, is_cuda=True, dtype=torch.FloatTensor):
+  v = torch.from_numpy(x).type(dtype)
+  if is_cuda:
+      v = v.cuda()
+  return v
+
+def load_net(fname, net, optimizer=None):
+  sp = torch.load(fname,map_location=torch.device('cpu'))
+  step = sp['step']
+  try:
+    learning_rate = sp['learning_rate']
+  except:
+    import traceback
+    traceback.print_exc()
+    learning_rate = 0.001
+  opt_state = sp['optimizer']
+  sp = sp['state_dict']
+  for k, v in net.state_dict().items():
+    try:
+      if k in sp:
+        param = sp[k]
+        v.copy_(param)
+        # print(v.size())
+      else:
+        # print(k)
+        # print(v.size())
+        v.copy_(torch.randn(v.size()))
+    except:
+      import traceback
+      traceback.print_exc()
+  
+  if optimizer is not None:  
+    try:
+      optimizer.load_state_dict(opt_state)
+    except:
+      import traceback
+      traceback.print_exc()
+  
+  print(fname)
+  return step, learning_rate 
+  
---
+++ test.py
@@ -0,0 +1,8 @@
+import cv2
+
+import cv2
+import numpy as np
+
+
+cv2.imshow('img', im)
+cv2.waitKey(10)
